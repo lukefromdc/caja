d73a0df6233bc6b2fe570fc539ec64430f4f6829
lukefromdc <lukefromdc@hushmail.com>
2016-07-11 21:19:44 -0400

    caja-main.c: exclude instance of unique from GTK3
    caja-main.c: exclude an instance of unique from GTK3 builds, leave it in GTk2 builds

--- src/caja-main.c
+++ src/caja-main.c
@@ -634,12 +634,13 @@ main (int argc, char *argv[])
          uris);
         g_strfreev (uris);
 
+#if !GTK_CHECK_VERSION(3, 0, 0)
         if (unique_app_is_running (application->unique_app) ||
                 kill_shell)
         {
             exit_with_last_window = TRUE;
         }
-
+#endif
         if (is_event_loop_needed ())
         {
             gtk_main ();

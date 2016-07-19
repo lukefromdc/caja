a8979e1a756497592e6bff084d545058bbcd9bc7
lukefromdc <lukefromdc@hushmail.com>
2016-07-19 03:01:55 -0400

    caja-window.c: Use GtkApplication API
    Based on https://github.com/GNOME/nautilus/commit/7500dd445df5a0cf082464f099169666727f6ec2

--- src/caja-window.c
+++ src/caja-window.c
@@ -32,7 +32,6 @@
 #include "caja-application.h"
 #include "caja-bookmarks-window.h"
 #include "caja-information-panel.h"
-#include "caja-main.h"
 #include "caja-window-manage-views.h"
 #include "caja-window-bookmarks.h"
 #include "caja-window-slot.h"
@@ -213,15 +212,6 @@ caja_window_init (CajaWindow *window)
     g_signal_connect_object (caja_signaller_get_current (), "popup_menu_changed",
                              G_CALLBACK (caja_window_load_extension_menus), window, G_CONNECT_SWAPPED);
 
-/* Keep the main event loop alive as long as the window exists */
-#if GTK_CHECK_VERSION(3, 0, 0)
-    /* FIXME: port to GtkApplication with GTK3 */
-    //gtk_quit_add_destroy (1, GTK_WIDGET (window));
-    caja_main_event_loop_register (GTK_WIDGET (window));
-#else
-    gtk_quit_add_destroy (1, GTK_OBJECT (window));
-    caja_main_event_loop_register (GTK_OBJECT (window));
-#endif
 }
 
 /* Unconditionally synchronize the GtkUIManager of WINDOW. */
@@ -1925,12 +1915,15 @@ caja_forget_history (void)
     CajaWindowSlot *slot;
     CajaNavigationWindowSlot *navigation_slot;
     GList *window_node, *l, *walk;
+	CajaApplication *app;
+
+	app = caja_application_dup_singleton ();
 
     /* Clear out each window's back & forward lists. Also, remove
      * each window's current location bookmark from history list
      * so it doesn't get clobbered.
      */
-    for (window_node = caja_application_get_window_list ();
+     for (window_node = gtk_application_get_windows (GTK_APPLICATION (app));
             window_node != NULL;
             window_node = window_node->next)
     {
@@ -1973,7 +1966,7 @@ caja_forget_history (void)
     free_history_list ();
 
     /* Re-add each window's current location to history list. */
-    for (window_node = caja_application_get_window_list ();
+	for (window_node = gtk_application_get_windows (GTK_APPLICATION (app));
             window_node != NULL;
             window_node = window_node->next)
     {
@@ -1992,6 +1985,8 @@ caja_forget_history (void)
             }
         }
     }
+
+	g_object_unref (app);
 }
 
 GList *
